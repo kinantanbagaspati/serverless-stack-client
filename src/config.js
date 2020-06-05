f629bf9f36544e81876b2fb4aa629868ccea0dea
Kinantan <kinantanyolo@gmail.com>
2020-06-05 04:37:55 -0700

    First commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-example-bucket2"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://9txtemsi6i.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_mzCSvHVAJ",
+        APP_CLIENT_ID: "2mpt8hdfs22s1547f8infmgeh1",
+        IDENTITY_POOL_ID: "us-east-1:f757e1e2-657d-44e4-a081-4b1186bd19a9"
+    }
+}
